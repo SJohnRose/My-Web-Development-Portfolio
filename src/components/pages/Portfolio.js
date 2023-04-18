38192eae4d50bc776b787556bfe5f91af8c819e5
SJohnRose <shamini.johnrose@gmail.com>
2023-04-18 10:48:32 +1000

    Starter nav tabs and components created

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1> My Projects</h1>
+      <p>
+        Portfolio page
+      </p>
+    </div>
+  );
+}
