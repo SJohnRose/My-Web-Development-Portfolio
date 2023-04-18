7e1e3b53798dbefd8a3b7af3fc884d56f580c3e4
SJohnRose <shamini.johnrose@gmail.com>
2023-04-18 11:50:31 +1000

    created contact form in contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,87 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import { validateEmail } from '../../utils/helpers';
+
 
 export default function Contact() {
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and message
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // if (!validateEmail(email) || !userName) {
+      if (!userName) {
+      setErrorMessage('Email or username is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      
+    }
+    
+    alert(`Hello ${userName}`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setMessage('');
+    setEmail('');
+  };
+
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Contact Form here
-      </p>
+      <h1>Contact Me </h1>
+      <form className="form">
+        User Name:
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="username"
+        />
+        Email:
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        Message:
+        <textarea
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          rows = {10}
+          cols = {60}
+          placeholder="Message"
+        />
+        
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
